523bef4fd365e52b10038f3a313c91c4a7a95c0e
w3villa-aman-kumar <aman.kumar@w3villa.com>
2023-06-29 15:33:16 +0530

    created edit route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,27 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+
+        if @article.save
+          flash[:notice] = "Article was created successfully."
+          redirect_to @article 
+        else
+          render :new
+        end
+    end
+
+    def update
+
+    end
+
 end
